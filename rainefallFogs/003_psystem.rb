12a9715ebb8a6d09c3dcca53f2c332fc6d8676f0
Riley Pierce <rileyraine01@gmail.com>
2024-11-26 12:55:01 +0000

    fix rf_reload_overlays in rainefallFogs

--- rainefallFogs/003_psystem.rb
+++ rainefallFogs/003_psystem.rb
@@ -3,7 +3,7 @@ if RfSettings::ADD_FOGS_TO_SETTINGS
     return if !$scene.is_a? Scene_Map
     $scene.spritesetGlobal.fog.visible = $PokemonSystem.disable_fogs.zero? if RfSettings::SETTINGS_AFFECT_FOGS
     $scene.spritesetGlobal.overlay.visible = $PokemonSystem.disable_fogs.zero? if RfSettings::SETTINGS_AFFECT_GLOBAL_OVERLAY
-    $map_factory.maps.each { |map| @spritesets[map.map_id].overlay.opacity = $PokemonSystem.disable_fogs.zero? ? 255 : RfSettings::SETTINGS_MAP_OVERLAY_OPACITY }
+    $map_factory.maps.each { |map| $scene.spriteset(map.map_id).overlay.opacity = $PokemonSystem.disable_fogs.zero? ? 255 : RfSettings::SETTINGS_MAP_OVERLAY_OPACITY }
   end
 
   class PokemonSystem
